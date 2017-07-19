f267066f7a74b288dfb4d74faa3e888e71813e15
ArbutusNate <arbutusnate@gmail.com>
2017-07-18 18:40:38 -0700

    Too many packages

--- ClozeDeleted.js
+++ ClozeDeleted.js
@@ -11,15 +11,6 @@ function ClozeDeleted(x, y, z){
   this.partialText = z
 }
 
-
-// function start(){
-//   var newCard = new ClozeDeleted(arg1, arg2);
-// }.then(function(data){
-//   console.log("fulltext: " + newCard.fullText);
-//   console.log("cloze: " + newcard.cloze);
-//   console.log("partial text: " + newCard.partialText);
-// });
-
 inquirer.prompt([
     {
       name: "fulltextInput",
@@ -31,14 +22,16 @@ inquirer.prompt([
   ]).then(function(answers){
     var testFull = answers.fulltextInput;
     var testDelete = answers.deletionInput;
-    var fullArray = testFull.split(" ");
-    var deleteArray = testDelete.split(" ");
+    // var fullArray = testFull.split(" ");
+    // var deleteArray = testDelete.split(" ");
+
     for(i = 0; i < fullArray.length; i++){
       if(deleteArray.includes(fullArray[i])){
         fullArray[i] = "...";
       }
       partial.concat(fullArray[i]);
     }
+
     console.log(partial)
     var newCard = new ClozeDeleted(answers.fulltextInput, answers.deletionInput, partial);
     console.log(newCard.fullText);
